9bf14e099b64ced51380b652b83bc2a07bdd4d87
Gin <gin@Gins-MacBookPro2020.local>
2023-04-17 13:56:06 +0700

    [BUILD] The first major release

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,6 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    ['react-native-reanimated/plugin'],
-    [
-      'module-resolver',
-      {
-        root: ['./src'],
-        extensions: ['.ts', '.tsx', '.js', 'jsx', '.ios.js', '.android.js'],
-        alias: {
-          '@app': './src',
-        },
-      },
-    ],
-  ],
+  plugins: [['react-native-reanimated/plugin']],
   env: {
     production: {
       plugins: ['transform-remove-console'], //removing consoles.log from app during release (production) versions
--- index.js
+++ index.js
@@ -3,12 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from '@app/index';
+import App from './src/components/atoms/EmptyComponent';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
-function helloNpm() {
-  return 'hello NPM';
-}
-
-module.exports = helloNpm;
